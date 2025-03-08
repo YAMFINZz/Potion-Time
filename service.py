4db52c9995786943de936e8f9e0943f75fef4691
YAMFINZz <peerutserm@gmail.com>
2025-03-08 20:32:34 +0700

    Update service.py

--- service.py
+++ service.py
@@ -1,14 +1,14 @@
+from time import sleep
+from class_libs import *
 from jnius import autoclass
 
 PythonService = autoclass(u'org.kivy.android.PythonService')
 PythonService.mService.setAutoRestartService(True)
 
-from time import sleep
-from class_libs import *
 
 while True:
-    DATA: dict = json.load(open(DATA_LOCATION, "r"))
     print("Service Running for it's life...")
+    DATA: dict = json.load(open(DATA_LOCATION, "r"))
     if (DATA['set_hour'] != None) and (DATA['set_min'] != None):
         isTimeSet: bool = True
         timeCalc(DATA['set_hour'], DATA['set_min'])

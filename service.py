aea992290ec5cb9f70cda939aed69832ed42dc16
YAMFINZz <peerutserm@gmail.com>
2025-03-08 16:51:09 +0700

    Update service.py

--- service.py
+++ service.py
@@ -5,15 +5,11 @@ PythonService.mService.setAutoRestartService(True)
 
 
 from time import sleep
-from threading import Thread
+from main import Condition, isTimeSet
 
-def messageService():
-    from main import Condition, isTimeSet
-    while True:
-        print(f'Service Checking | isTimeSet : {isTimeSet}')
-        if isTimeSet and Condition().isTimeButtonReady() and (Condition().timeCon(1) or Condition().timeCon(2)):
-            if Condition().isMessageReady():
-                Condition().sendMessage()
-        sleep(1)
-
-Thread(target=messageService, daemon=True).start()
+while True:
+    print(f'Service Checking | isTimeSet : {isTimeSet}')
+    if isTimeSet and Condition().isTimeButtonReady() and (Condition().timeCon(1) or Condition().timeCon(2)):
+        if Condition().isMessageReady():
+            Condition().sendMessage()
+    sleep(1)

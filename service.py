b8d33c1f821dce0d2956f0037b5eb7921e25c3ce
YAMFINZz <peerutserm@gmail.com>
2025-03-09 20:51:27 +0700

    test

--- service.py
+++ service.py
@@ -14,21 +14,17 @@ def isTimeSet() -> bool:
 def isMessageReady() -> bool:
     return (DATA['last_time_msg_sent'] == 0) or (int(time()) - (DATA['last_time_msg_sent']) > 300)
 
-def sendMessage() -> None:
+def sendMessage():
     DATA.update({'last_time_msg_sent': int(time())})
     json.dump(DATA, open(DATA_LOCATION, 'w'))
-    from plyer.platforms.android.notification import AndroidNotification
-    AndroidNotification().notify(title = 'Potion TIME!', message = f'📅It\'s {DATA["streak_month"]} month and {DATA["streak_day"]} day📅\n🤍Come here and Check-in🤍',app_icon = 'assets/img/icon/icon.ico')
+    from plyer import notification
+    notification.notify(title = 'Potion TIME!', message = f'📅It\'s {DATA["streak_month"]} month and {DATA["streak_day"]} day📅\n🤍Come here and Check-in🤍',app_icon = 'assets/img/icon/icon.ico')
 
 while True:
     DATA: dict = json.load(open(DATA_LOCATION, "r"))
     
-    print(f'isTimeSet() : {isTimeSet()}')
-    print(f'Condition().isTimeButtonReady() : {Condition().isTimeButtonReady()}')
-    print(f'(Condition().timeCon(1, 2) : {(Condition().timeCon(1) or Condition().timeCon(2))}')
-    print(f'isMessageReady() : {isMessageReady()}')
-    
     if (isTimeSet() and Condition().isTimeButtonReady()) and (Condition().timeCon(1) or Condition().timeCon(2)):
         if isMessageReady():
-            sendMessage()
+            try: sendMessage()
+            except AttributeError: print("ATTR ERROR")
     sleep(5)
